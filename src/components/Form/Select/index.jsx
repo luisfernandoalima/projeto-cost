bdd7bb1da42804d7a6b7805f95d4e8995ecc7ad0
Luis Lima <luisfernando.lima2005@gmail.com>
2024-12-11 20:46:23 -0300

    Utilizando API

--- src/components/Form/Select/index.jsx
+++ src/components/Form/Select/index.jsx
@@ -4,8 +4,16 @@ export const Select = ({ text, name, options, handleOnChange, value }) => {
   return (
     <div className={styles.form_control}>
       <label htmlFor={name}>{text}</label>
-      <select name={name} id={name}>
+      <select
+        name={name}
+        id={name}
+        onChange={handleOnChange}
+        value={value || ""}
+      >
         <option>Selecione uma opção</option>
+        {options.map((option) => (
+          <option key={option.id}>{option.name}</option>
+        ))}
       </select>
     </div>
   );
--- src/pages/NewProject/index.jsx
+++ src/pages/NewProject/index.jsx
@@ -1,13 +1,37 @@
-import { ProjectForm } from '../../components/Project/Form';
+import { useNavigate } from "react-router-dom";
 
-import styles from './NewProject.module.css'
+import { ProjectForm } from "../../components/Project/Form";
+
+import styles from "./NewProject.module.css";
 
 export const NewProject = () => {
+  const navigate = useNavigate();
+
+  const createProject = (project) => {
+    // initialize cost and services
+
+    project.cost = 0;
+    project.services = [];
+
+    fetch("http://localhost:5000/projects", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(project)
+    })
+      .then((response) => response.json())
+      .then((data) =>
+        navigate("/project", { message: "Projeto criado com sucesso!" })
+      )
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className={styles.newproject_container}>
       <h1>Criar Projeto</h1>
       <p>Crie seu projeto para depois adicionar os serviços</p>
-      <ProjectForm btnText="Criar Projeto"/>
+      <ProjectForm btnText="Criar Projeto" handleSubmit={createProject} />
     </div>
   );
 };
