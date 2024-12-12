508d9a589ee7626d5c8e557a76dd77ace8e2bbdf
Luis Lima <luisfernando.lima2005@gmail.com>
2024-12-12 17:37:17 -0300

    Criação dos cards

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,19 +1,58 @@
-import { useLocation } from "react-router-dom"
-import { Message } from "../../components/layouts/Message"
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+import { Message } from "../../components/layouts/Message";
+import { Container } from "../../components/layouts/Container";
+import { LinkButton } from "../../components/layouts/LinkButton";
+import { ProjectCard } from "../../components/Project/Card";
+
+import styles from "./Projects.module.css";
 
 export const Projects = () => {
+  const [projects, setProjects] = useState([]);
 
-    const location = useLocation()
-    let message = ''
-    if(location.state){
-        message = location.state.message
-    }
+  useEffect(() => {
+    fetch("http://localhost:5000/projects", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setProjects(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-    return(
-        <div>
-            <h1>Meus Projetos</h1>
+  const location = useLocation();
+  let message = "";
+  if (location.state) {
+    message = location.state.message;
+  }
 
-            {message && <Message type="success" msg={message}/>}
-        </div>
-    )
-}
+  return (
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton text="Novo Projeto" to="/newproject" />
+      </div>
+      {message && <Message type="success" msg={message} />}
+      <Container customClass="start">
+        {projects.length > 0 &&
+          projects.map((project) => (
+            <ProjectCard
+              name={project.name}
+              id={project.id}
+              budget={project.budget}
+              category={project.category.name}
+              key={project.id}
+            />
+          ))}
+
+        {/* <p>Projetos...</p> */}
+      </Container>
+    </div>
+  );
+};
